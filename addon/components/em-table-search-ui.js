1d63b084d0d3fdf6a4fd8a31209766ed53aca911
Sreenath Somarajapuram <sree@apache.org>
2015-10-27 09:30:02 -0700

    Added search ui

---
+++ addon/components/em-table-search-ui.js
@@ -0,0 +1,33 @@
+import Ember from 'ember';
+import layout from '../templates/components/em-table-search-ui';
+
+export default Ember.Component.extend({
+  layout: layout
+
+  classNames: ['search-view'],
+
+  text: '',
+  _boundText: function () {
+    return this.get('text') || '';
+  }.property(),
+
+  _validRegEx: function () {
+    var regExText = this.get('_boundText');
+    regExText = regExText.substr(regExText.indexOf(':') + 1);
+    try {
+      new RegExp(regExText, 'im');
+    }
+    catch(e) {
+      return false;
+    }
+    return true;
+  }.property('_boundText'),
+
+  actions: {
+    search: function () {
+      if(this.get('_validRegEx')) {
+        this.get('parentView').send('search', this.get('_boundText'));
+      }
+    }
+  }
+});
