40a450e6d7e74ad8b61de7ec21171456eabed86f
Sreenath Somarajapuram <sree@apache.org>
2015-11-30 14:24:37 +0530

    Adding table definition

--- addon/utils/table-definition.js
+++ addon/utils/table-definition.js
@@ -1,14 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Object.extend({
+  enableSearch: true,
+  enableSort: true,
+  enablePagination: false,
+  enableColumnResize: true,
+
   sortColumnId: '',
   sortOrder: '',
 
-  searchText: '',
-  searchColumnNames: null,
+  searchText: 'A',
 
   pageNum: 1,
   rowCount: 10,
   rowCountOptions: [5, 10, 25, 50, 100],
-  pageNavOnFooterAt: 25,
+
+  minRowsForFooter: 25,
+
+  columns: []
 });
