08f993f05e482b0886f053b94f3d95790edc5cb0
Sreenath Somarajapuram <sree@apache.org>
2015-12-28 18:26:59 +0530

    Adding definition UTs

--- tests/unit/utils/column-definition-test.js
+++ tests/unit/utils/column-definition-test.js
@@ -1,9 +1,61 @@
+import Ember from 'ember';
 import ColumnDefinition from '../../../utils/column-definition';
 import { module, test } from 'qunit';
 
 module('Unit | Utility | column definition');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
+test('Class creation test', function(assert) {
   assert.ok(ColumnDefinition);
+
+  assert.ok(ColumnDefinition.make);
+});
+
+test('make - Instance creation test', function(assert) {
+
+  var definition = ColumnDefinition.make({
+    id: "testId"
+  });
+  var definitions = ColumnDefinition.make([{
+    id: "testId 1"
+  },{
+    id: "testId 2"
+  }]);
+
+  // Single
+  assert.ok(definition);
+
+  // Multiple
+  assert.ok(definitions);
+  assert.ok(Array.isArray(definitions));
+  assert.equal(definitions.length, 2);
+});
+
+test('make - Instance creation failure test', function(assert) {
+  assert.throws(function () {
+    ColumnDefinition.make({});
+  });
+});
+
+test('Instance test', function(assert) {
+  var definition = ColumnDefinition.make({
+    id: "testId",
+    contentPath: "a.b"
+  });
+  var data = Ember.Object.create({
+    a: {
+      b: 42
+    }
+  });
+
+  assert.ok(definition.getCellContent);
+  assert.ok(definition.getSearchValue);
+  assert.ok(definition.getSortValue);
+
+  assert.equal(definition.id, "testId");
+  assert.equal(definition.headerTitle, "Not Available!");
+  assert.equal(definition.contentPath, "a.b");
+
+  assert.equal(definition.getCellContent(data), 42);
+  assert.equal(definition.getSearchValue(data), 42);
+  assert.equal(definition.getSortValue(data), 42);
 });
--- tests/unit/utils/table-definition-test.js
+++ tests/unit/utils/table-definition-test.js
@@ -3,7 +3,32 @@ import { module, test } from 'qunit';
 
 module('Unit | Utility | table definition');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
+test('Class creation test', function(assert) {
   assert.ok(TableDefinition);
+});
+
+test('Default instance test', function(assert) {
+  var definition = TableDefinition.create();
+
+  assert.ok(definition);
+
+  assert.equal(definition.pageNum, 1);
+  assert.equal(definition.rowCount, 10);
+  assert.equal(definition.minRowsForFooter, 25);
+});
+
+test('Page-num reset test', function(assert) {
+  var definition = TableDefinition.create();
+
+  assert.equal(definition.pageNum, 1);
+
+  definition.set("pageNum", 5);
+  assert.equal(definition.pageNum, 5);
+
+  definition.set("searchText", "x");
+  assert.equal(definition.pageNum, 1);
+
+  definition.set("pageNum", 5);
+  definition.set("rowCount", 5);
+  assert.equal(definition.pageNum, 1);
 });
