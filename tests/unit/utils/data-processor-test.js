22eb0b6c210611d54ae11bd5ebb705ebc9a5a57b
Sreenath Somarajapuram <sree@apache.org>
2015-12-29 10:14:45 +0530

    Improving processor tests

--- tests/unit/utils/data-processor-test.js
+++ tests/unit/utils/data-processor-test.js
@@ -1,9 +1,30 @@
+import Ember from 'ember';
+
 import DataProcessor from '../../../utils/data-processor';
 import { module, test } from 'qunit';
 
 module('Unit | Utility | data processor');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
+test('Class creation test', function(assert) {
   assert.ok(DataProcessor);
+});
+
+test('Instance default test', function(assert) {
+  var processor;
+
+  Ember.run(function () {
+    processor = DataProcessor.create({
+      tableDefinition: Ember.Object.create(),
+      startSearch: function () {
+        // Test Search
+      },
+      startSort: function () {
+        // Test Sort
+      }
+    });
+  });
+
+  assert.ok(processor);
+  assert.equal(processor.get('isSorting'), false);
+  assert.equal(processor.get('isSearching'), false);
 });
